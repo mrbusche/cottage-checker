78d0d0670ee838d8d4a53e4b3cb06a43a3aeccff
Matt Busche <mrbusche@gmail.com>
2020-07-23 22:33:21 -0500

    Initial commit (#1)

---
+++ src/main/java/com/webpage/checker/CheckerController.java
@@ -0,0 +1,24 @@
+package com.webpage.checker;
+
+import com.webpage.checker.helper.CheckerHelper;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.select.Elements;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.io.IOException;
+
+@RestController
+public class CheckerController {
+
+    @GetMapping("/index")
+    public String index() throws IOException {
+        String url = Checker.TWO_BEDROOM.getUrl();
+        //String url = Checker.THREE_BEDROOM.getUrl();
+        Document doc = Jsoup.connect(url).get();
+        Elements els = doc.getElementsByClass("property-calendar-extreme-outer mrg-left-0 mrg-right-neg-50 row");
+        return CheckerHelper.removeStuff(els.toString());
+    }
+
+}
