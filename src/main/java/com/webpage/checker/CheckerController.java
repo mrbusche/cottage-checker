77054d722136fdf2adaaabfa18ecf68c01f55ede
Matt Busche <mrbusche@gmail.com>
2020-08-07 21:34:34 -0500

    updated html

--- src/main/java/com/webpage/checker/CheckerController.java
+++ src/main/java/com/webpage/checker/CheckerController.java
@@ -12,9 +12,16 @@ import java.io.IOException;
 @RestController
 public class CheckerController {
 
-    @GetMapping("/index")
-    public String index() throws IOException {
-//        String url = Checker.TWO_BEDROOM;
+    @GetMapping("/two")
+    public String two() throws IOException {
+        String url = Checker.TWO_BEDROOM;
+        Document doc = Jsoup.connect(url).get();
+        Elements els = doc.getElementsByClass("property-calendar-extreme-outer mrg-left-0 mrg-right-neg-50 row");
+        return CheckerHelper.removeStuff(els.toString());
+    }
+
+    @GetMapping("/three")
+    public String three() throws IOException {
         String url = Checker.THREE_BEDROOM;
         Document doc = Jsoup.connect(url).get();
         Elements els = doc.getElementsByClass("property-calendar-extreme-outer mrg-left-0 mrg-right-neg-50 row");
