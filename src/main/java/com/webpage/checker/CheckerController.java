62212183310e3e46c4684034f3f082dee80e870c
Matt Busche <mrbusche@gmail.com>
2020-08-20 20:55:39 -0500

    Add review count and some refactoring

--- src/main/java/com/webpage/checker/CheckerController.java
+++ src/main/java/com/webpage/checker/CheckerController.java
@@ -1,9 +1,6 @@
 package com.webpage.checker;
 
-import com.webpage.checker.helper.CheckerHelper;
-import org.jsoup.Jsoup;
-import org.jsoup.nodes.Document;
-import org.jsoup.select.Elements;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,21 +9,16 @@ import java.io.IOException;
 @RestController
 public class CheckerController {
 
-    private final static String htmlElement = "property-calendar-extreme-outer mrg-left-0 mrg-right-neg-50 row";
+    @Autowired
+    CheckerService checkerService;
 
     @GetMapping("/two")
     public String two() throws IOException {
-        String url = Checker.TWO_BEDROOM;
-        Document doc = Jsoup.connect(url).get();
-        Elements els = doc.getElementsByClass(htmlElement);
-        return CheckerHelper.removeStuff(els.toString());
+        return checkerService.retrieveHTML(Checker.TWO_BEDROOM);
     }
 
     @GetMapping("/three")
     public String three() throws IOException {
-        String url = Checker.THREE_BEDROOM;
-        Document doc = Jsoup.connect(url).get();
-        Elements els = doc.getElementsByClass(htmlElement);
-        return CheckerHelper.removeStuff(els.toString());
+        return checkerService.retrieveHTML(Checker.THREE_BEDROOM);
     }
 }
