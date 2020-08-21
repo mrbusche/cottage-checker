0941d29a2e7de3087aecaab151fe213d0519f23a
Matt Busche <mrbusche@gmail.com>
2020-08-20 21:08:00 -0500

    More cleanup

--- src/main/java/com/webpage/checker/CheckerApplication.java
+++ src/main/java/com/webpage/checker/CheckerApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class CheckerApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(CheckerApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(CheckerApplication.class, args);
+    }
 
 }
