7d5deabfe384f5560c9ece1bca6afb053f0c7e03
Matt Busche <mrbusche@gmail.com>
2020-08-20 21:08:00 -0500

    More cleanup

--- src/main/java/com/webpage/checker/Checker.java
+++ src/main/java/com/webpage/checker/Checker.java
@@ -3,6 +3,6 @@ package com.webpage.checker;
 public class Checker {
     public static final String THREE_BEDROOM = "https://www.vacationinsouthhaven.com/vacation-rental-home.asp?PageDataID=160236";
     public static final String TWO_BEDROOM = "https://www.vacationinsouthhaven.com/vacation-rental-home.asp?PageDataID=168702";
-    public final static String CALENDAR_ELEMENT = "property-calendar-extreme-outer mrg-left-0 mrg-right-neg-50 row";
-    public final static String REVIEWS_ELEMENT = "testimonial-wrapper";
+    public static final String CALENDAR_ELEMENT = "property-calendar-extreme-outer mrg-left-0 mrg-right-neg-50 row";
+    public static final String REVIEWS_ELEMENT = "testimonial-wrapper";
 }
--- src/main/java/com/webpage/checker/CheckerController.java
+++ src/main/java/com/webpage/checker/CheckerController.java
@@ -1,5 +1,6 @@
 package com.webpage.checker;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,10 +8,10 @@ import org.springframework.web.bind.annotation.RestController;
 import java.io.IOException;
 
 @RestController
+@RequiredArgsConstructor
 public class CheckerController {
 
-    @Autowired
-    CheckerService checkerService;
+    private final CheckerService checkerService;
 
     @GetMapping("/two")
     public String two() throws IOException {
