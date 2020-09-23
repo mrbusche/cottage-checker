cd293b2ae2b9058a15bd1cce8509a9fc3a7a6d15
Matt Busche <mrbusche@gmail.com>
2020-09-22 20:23:44 -0500

    clean up code smells

--- src/main/java/com/webpage/checker/Checker.java
+++ src/main/java/com/webpage/checker/Checker.java
@@ -1,6 +1,10 @@
 package com.webpage.checker;
 
 public class Checker {
+    private Checker() {
+        throw new IllegalStateException("Utility class");
+    }
+
     public static final String THREE_BEDROOM = "https://www.vacationinsouthhaven.com/vacation-rental-home.asp?PageDataID=160236";
     public static final String TWO_BEDROOM = "https://www.vacationinsouthhaven.com/vacation-rental-home.asp?PageDataID=168702";
     public static final String CALENDAR_ELEMENT = "property-calendar-extreme-outer mrg-left-0 mrg-right-neg-50 row";
--- src/main/java/com/webpage/checker/helper/CheckerHelper.java
+++ src/main/java/com/webpage/checker/helper/CheckerHelper.java
@@ -1,6 +1,10 @@
 package com.webpage.checker.helper;
 
 public class CheckerHelper {
+    private CheckerHelper() {
+        throw new IllegalStateException("Utility class");
+    }
+
     public static String removeStuff(String s) {
         return s.replace("\n", "")
                 .replace("\r", "")
