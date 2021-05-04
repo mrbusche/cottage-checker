24ddf7ffacb8b7c1294639a6a0d22cf610bd4d1d
Matt Busche <mrbusche@gmail.com>
2021-05-03 21:38:39 -0500

    Update CheckerApplicationTests.groovy (#141)

--- src/test/groovy/com/webpage/checker/CheckerApplicationTests.groovy
+++ src/test/groovy/com/webpage/checker/CheckerApplicationTests.groovy
@@ -46,7 +46,7 @@ class CheckerApplicationTests extends Specification {
         month      | index | bookings
         "May"      | 0     | 0
         "June"     | 1     | 8
-        "July"     | 2     | 21
+        "July"     | 2     | 25
         "August"   | 3     | 24
         "September"| 4     | 0
         "October"  | 5     | 0
