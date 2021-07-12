c07d879cf9f9f8fbc67c14f952d5c9327a2b4427
Matt Busche <mrbusche@gmail.com>
2021-07-11 21:06:11 -0500

    add retries since website sucks (#177)

--- src/main/java/com/webpage/checker/CheckerService.java
+++ src/main/java/com/webpage/checker/CheckerService.java
@@ -12,23 +12,43 @@ import java.util.ArrayList;
 
 @Service
 public class CheckerService {
+    int maxRetries = 10;
+    int timeout = 30000;
 
     int retrieveReviewCount(String url) throws IOException {
-        Document doc = Jsoup.connect(url).get();
-        Elements els = doc.getElementsByClass(Checker.REVIEWS_ELEMENT);
-        return els.size();
+        int retries = 0;
+        while (retries < maxRetries) {
+            try {
+                Document doc = Jsoup.connect(url).timeout(timeout).get();
+                Elements els = doc.getElementsByClass(Checker.REVIEWS_ELEMENT);
+                retries = maxRetries;
+                return els.size();
+            } catch (IOException e) {
+                retries++;
+            }
+        }
+        return 0;
     }
 
     ArrayList<Integer> retrieveRoomNights(String url) throws IOException {
-        Document doc = Jsoup.connect(url).get();
-        Elements els = doc.getElementsByClass("table property-calendar-table");
-        ArrayList<Integer> counts = new ArrayList<>();
-        for (Element element : els) {
-            int count = StringUtils.countMatches(element.toString(), "background-color:#FFCC66;");
-            count += StringUtils.countMatches(element.toString(), "background: url('https://cdn.liverez.com/0/images/yahoo-calendar-departure-bg.gif')");
-            count += StringUtils.countMatches(element.toString(), "background: url('https://cdn.liverez.com/0/images/yahoo-calendar-overlap-bg.gif')");
-            counts.add(count);
+        int retries = 0;
+        while (retries < maxRetries) {
+            try {
+                Document doc = Jsoup.connect(url).timeout(timeout).get();
+                Elements els = doc.getElementsByClass("table property-calendar-table");
+                ArrayList<Integer> counts = new ArrayList<>();
+                for (Element element : els) {
+                    int count = StringUtils.countMatches(element.toString(), "background-color:#FFCC66;");
+                    count += StringUtils.countMatches(element.toString(), "background: url('https://cdn.liverez.com/0/images/yahoo-calendar-departure-bg.gif')");
+                    count += StringUtils.countMatches(element.toString(), "background: url('https://cdn.liverez.com/0/images/yahoo-calendar-overlap-bg.gif')");
+                    counts.add(count);
+                }
+                retries = maxRetries;
+                return counts;
+            } catch (IOException e) {
+                retries++;
+            }
         }
-        return counts;
+        return new ArrayList<>();
     }
 }
