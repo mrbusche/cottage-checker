fc12565bad26e1d321a7ad54ddade7c5024d47d6
Matt Busche <mrbusche@gmail.com>
2021-07-14 13:28:58 -0500

    Update CheckerApplicationTests.groovy (#180)

--- src/test/groovy/com/webpage/checker/CheckerApplicationTests.groovy
+++ src/test/groovy/com/webpage/checker/CheckerApplicationTests.groovy
@@ -10,7 +10,7 @@ class CheckerApplicationTests extends Specification {
         when:
         int reviewCount = checkerService.retrieveReviewCount(Checker.COZY_COTTAGE)
         then:
-        reviewCount == 19
+        reviewCount == 20
     }
 
     def "test Haven Hideaway reviews"() throws IOException {
