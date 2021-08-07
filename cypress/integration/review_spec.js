afdf69666dc91dbd48c10183a03e3e9e54d30605
Matt Busche <mrbusche@gmail.com>
2021-08-06 22:26:51 -0500

    add cypress

---
+++ cypress/integration/review_spec.js
@@ -0,0 +1,11 @@
+describe('check cottages', () => {
+    it('check cozy cottage vrbo reviews', () => {
+        cy.visit('https://www.vrbo.com/2111572');
+        cy.contains('5 Reviews');
+    })
+
+    it('check cozy cottage vrbo', () => {
+        cy.visit('https://www.vrbo.com/2111556');
+        cy.contains('5 Reviews');
+    })
+})
