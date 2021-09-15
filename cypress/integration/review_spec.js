2061cd0f6534bf50b25ccf6cd97af396fe2b8674
Matt Busche <mrbusche@gmail.com>
2021-09-15 16:40:41 -0500

    Update review_spec.js

--- cypress/integration/review_spec.js
+++ cypress/integration/review_spec.js
@@ -1,10 +1,10 @@
 describe('check cottages', () => {
     it('check cozy cottage vrbo reviews', () => {
         cy.visit('https://www.vrbo.com/2111572');
-        cy.contains('5 Reviews');
+        cy.contains('6 Reviews');
     })
 
-    it('check cozy cottage vrbo', () => {
+    it('check haven hideaway vrbo reviews', () => {
         cy.visit('https://www.vrbo.com/2111556');
         cy.contains('6 Reviews');
     })
