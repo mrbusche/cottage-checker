bd5312224b4329f9e622f1017fd2bd8793157b28
Matt Busche <mrbusche@gmail.com>
2021-12-15 21:51:26 -0600

    vrbo updated styles (#257)

--- cypress/integration/review_spec.js
+++ cypress/integration/review_spec.js
@@ -1,18 +1,12 @@
 describe('check cottage reviews', () => {
     it('check cozy cottage vrbo reviews', () => {
         cy.visit('https://www.vrbo.com/2111572');
-        cy.get('.reviews-summary__num-reviews.text-link').first().should(($div) => {
-            const text = $div.text();
-            expect(text).to.eq('7 Reviews');
-        });
+        cy.get('.text-link').first().should('have.text', '7 Reviews');
     })
 
     it('check haven hideaway vrbo reviews', () => {
         cy.visit('https://www.vrbo.com/2111556');
-        cy.get('.reviews-summary__num-reviews.text-link').first().should(($div) => {
-            const text = $div.text();
-            expect(text).to.eq('6 Reviews');
-        });
+        cy.get('.text-link').first().should('have.text', '6 Reviews');
     })
 })
 
