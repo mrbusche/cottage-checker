d91b84494aab61ef8632f368564af57a2f15bd96
Matt Busche <mrbusche@gmail.com>
2022-06-23 17:21:34 -0500

    Update review.cy.js

--- cypress/e2e/review.cy.js
+++ cypress/e2e/review.cy.js
@@ -1,7 +1,7 @@
 describe('check cottage reviews', () => {
     it('check cozy cottage vrbo reviews', () => {
         cy.visit('https://www.vrbo.com/2111572');
-        cy.get('.text-link').first().should('contain.text', '17 Reviews');
+        cy.get('.text-link').first().should('contain.text', '18 Reviews');
     })
 
     it('check haven hideaway vrbo reviews', () => {
