434b2db803041df1c0d51305520cd93cdfe40c0b
Matt Busche <mrbusche@gmail.com>
2022-07-23 09:26:47 -0500

    remove unused tests

--- src/test/groovy/com/webpage/checker/CheckerApplicationTests.groovy
+++ src/test/groovy/com/webpage/checker/CheckerApplicationTests.groovy
@@ -6,51 +6,51 @@ import spock.lang.Unroll
 class CheckerApplicationTests extends Specification {
     CheckerService checkerService = new CheckerService()
 
-    def "test Cozy Cottage reviews"() throws IOException {
-        when:
-        int reviewCount = checkerService.retrieveReviewCount(Checker.COZY_COTTAGE)
-        then:
-        reviewCount == 25
-    }
+    // def "test Cozy Cottage reviews"() throws IOException {
+    //     when:
+    //     int reviewCount = checkerService.retrieveReviewCount(Checker.COZY_COTTAGE)
+    //     then:
+    //     reviewCount == 25
+    // }
 
-    def "test Haven Hideaway reviews"() throws IOException {
-        when:
-        int reviewCount = checkerService.retrieveReviewCount(Checker.HAVEN_HIDEAWAY)
-        then:
-        reviewCount == 11
-    }
+    // def "test Haven Hideaway reviews"() throws IOException {
+    //     when:
+    //     int reviewCount = checkerService.retrieveReviewCount(Checker.HAVEN_HIDEAWAY)
+    //     then:
+    //     reviewCount == 11
+    // }
 
-    @Unroll
-    def "test Cozy Cottage nights #month"() throws IOException {
-        ArrayList<Integer> counts = checkerService.retrieveRoomNights(Checker.COZY_COTTAGE)
-        expect:
-        counts.get(index) == bookings
-        where:
-        month      | index | bookings
-        "April"    | 0     | 0
-        "May"      | 1     | 15
-        "June"     | 2     | 19
-        "July"     | 3     | 28
-        "August"   | 4     | 20
-        "September"| 5     | 0
-        "October"  | 6     | 0
-        "November" | 7     | 0
-    }
+    // @Unroll
+    // def "test Cozy Cottage nights #month"() throws IOException {
+    //     ArrayList<Integer> counts = checkerService.retrieveRoomNights(Checker.COZY_COTTAGE)
+    //     expect:
+    //     counts.get(index) == bookings
+    //     where:
+    //     month      | index | bookings
+    //     "April"    | 0     | 0
+    //     "May"      | 1     | 15
+    //     "June"     | 2     | 19
+    //     "July"     | 3     | 28
+    //     "August"   | 4     | 20
+    //     "September"| 5     | 0
+    //     "October"  | 6     | 0
+    //     "November" | 7     | 0
+    // }
 
-    @Unroll
-    def "test Haven Hideaway nights #month"() throws IOException {
-        ArrayList<Integer> counts = checkerService.retrieveRoomNights(Checker.HAVEN_HIDEAWAY)
-        expect:
-        counts.get(index) == bookings
-        where:
-        month      | index | bookings
-        "April"    | 0     | 0
-        "May"      | 1     | 8
-        "June"     | 2     | 4
-        "July"     | 3     | 19
-        "August"   | 4     | 14
-        "September"| 5     | 5
-        "October"  | 6     | 0
-        "November" | 7     | 0
-    }
+    // @Unroll
+    // def "test Haven Hideaway nights #month"() throws IOException {
+    //     ArrayList<Integer> counts = checkerService.retrieveRoomNights(Checker.HAVEN_HIDEAWAY)
+    //     expect:
+    //     counts.get(index) == bookings
+    //     where:
+    //     month      | index | bookings
+    //     "April"    | 0     | 0
+    //     "May"      | 1     | 8
+    //     "June"     | 2     | 4
+    //     "July"     | 3     | 19
+    //     "August"   | 4     | 14
+    //     "September"| 5     | 5
+    //     "October"  | 6     | 0
+    //     "November" | 7     | 0
+    // }
 }
